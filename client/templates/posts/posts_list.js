412e79fe19e238f8bef14f59bbbb5e0d164fe882
ddelimar <ddelimar@domdelimar.fake>
2015-02-09 01:15:12 +0100

    added some extra comments to clarify things

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,5 +1,11 @@
 Template.postsList.helpers({
   posts: function() {
+  	/*
+	Find & Fetch
+	In Meteor, find() returns a cursor, which is a reactive data source. When we want to log its contents, we can then use fetch() on that cursor to transform it into an array .
+	
+	Within an app, Meteor is smart enough to know how to iterate over cursors without having to explicitly convert them into arrays first. This is why you won't see fetch() that often in actual Meteor code (and why we didn't use it in the above example).
+  	*/
   	return Posts.find();
   }
 });
